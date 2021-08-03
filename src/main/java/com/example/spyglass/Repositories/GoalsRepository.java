5efde6a5eccac41503a96777e7313d8c30e40ec8
AntaNdiaye <ndiayenyocisse@gmail.com>
2021-08-03 03:06:58 -0400

    Init push

--- src/main/java/com/example/spyglass/Repositories/GoalsRepository.java
+++ src/main/java/com/example/spyglass/Repositories/GoalsRepository.java
@@ -1,10 +1,13 @@
 package com.example.spyglass.Repositories;
 
 import com.example.spyglass.Model.Goals;
+import com.example.spyglass.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GoalsRepository extends JpaRepository <Goals,Long> {
-
+    List<Goals> findAllByUser(User user);
 }
--- src/main/java/com/example/spyglass/Services/GoalService.java
+++ src/main/java/com/example/spyglass/Services/GoalService.java
@@ -1,13 +1,56 @@
 package com.example.spyglass.Services;
 
+import com.example.spyglass.Exceptions.GoalAlreadyExistsExeption;
+import com.example.spyglass.Exceptions.GoalNotFoundException;
+import com.example.spyglass.Model.Goals;
 import com.example.spyglass.Repositories.GoalsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GoalService {
 
     @Autowired
-    GoalsRepository goalsRepository;
+    GoalsRepository goalsRepo;
 
+    public GoalService(){
+
+    }
+
+    public Goals creatGoal(Goals newGoal) throws GoalAlreadyExistsExeption {
+        if (goalsRepo.existsById(newGoal.getGoalId())){
+            throw new GoalAlreadyExistsExeption(newGoal.getGoalId());
+        }
+        return goalsRepo.save(newGoal);
+    }
+
+    public List<Goals> getAllGoals(){
+        return goalsRepo.findAll();
+    }
+
+    public Goals findGoal(Long id) throws GoalNotFoundException{
+        if(goalsRepo.existsById(id)){
+            return goalsRepo.findById(id).orElseThrow(()
+                         -> new GoalNotFoundException(id));
+        }
+        throw new GoalNotFoundException(id);
+    }
+
+    public Boolean deleteGoal(Long id) throws GoalNotFoundException{
+        if (goalsRepo.existsById(id)){
+            goalsRepo.deleteById(id);
+            return true;
+        }
+        throw new GoalNotFoundException(id);
+    }
+
+    public void updateGoal(Long id, Optional<Goals> updatedGoal) throws GoalNotFoundException{
+        if (goalsRepo.existsById(id)){
+            Optional<Goals> oldGoal = goalsRepo.findById(id);
+             oldGoal = updatedGoal;
+        }
+    }
 }

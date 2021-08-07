6ce2a6ba30f253963a980e23cd6118fa60ab99c6
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2021-08-07 11:37:42 -0400

    status bar and number animation

--- spy_glass_frontend-app/src/components/goalCardComponent/GoalCard.js
+++ spy_glass_frontend-app/src/components/goalCardComponent/GoalCard.js
@@ -3,18 +3,31 @@ import "../createGoalComponent/CreateGoal"
 import CreateGoal from "../createGoalComponent/CreateGoal";
 import React, {useState, useEffect} from 'react';
 
+import ProgressBar from "@ramonak/react-progress-bar";
+import { CountUp } from 'use-count-up'
+
 const GoalCard = (props) => 
 {
+
+    const [completedAmount, setCompletedAmount] = useState(0); // should be 0 innitially 
+
+    
+    useEffect(() => 
+    {
+        setCompletedAmount(10); // 20,000 of 200,0000  (default display values) = 10% 
+    }, [])
 
     const clicked = () =>
     {
         props.onEdit();
+
+        //setCompletedAmount(50);  this is a test to show bar of that goal going to 50. After caluclating the required value this should be called 
     }
 
 
     return (
         <div className="goalCard">
-            {}
+            
                 <div className="goalCardHeader">
                     <h2>House</h2>
                     <p onClick={clicked}>Edit</p>
@@ -34,19 +47,16 @@ const GoalCard = (props) =>
                     </div>
                 </div>
 
-                <div className="progressBar">
-                    <div className="progressBarCurrent">
-                      
+                <div>
+                    <ProgressBar completed={completedAmount} transitionDuration="2s" bgColor="#04D582" borderRadius="5px" baseBgColor="#E3F3FD" isLabelVisible={false} height="15px"/>
                     </div>
-                    <div className="progressBarFinish">
-                      
-                    </div>
-                </div>
 
                 <div className="priceContainer">
-                    <p>$20,000</p>
+                    <CountUp isCounting thousandsSeparator="," start={0} end={20000} duration={2}></CountUp>
                     <p>$200,000</p>
                 </div>
+
+
 
             <div className="cardAchieveContainer">
                 <p>Achieve your goal on July 2027 by saving $500 each month</p>
@@ -54,6 +64,9 @@ const GoalCard = (props) =>
         </div>
     )
 }
+
+
+
 
 
 export default GoalCard; 
